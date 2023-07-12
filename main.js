df8cf8cc5549c6a0ca7ddffec122577bdd42cfa8
Mariano Lasarte <marianolasarte@hotmail.com.ar>
2023-07-12 20:00:30 -0300

    frist commit

--- main.js
+++ main.js
@@ -2,22 +2,25 @@ let cuota = 0
 let montototal = 0
 
 let monto = parseFloat(prompt("ingrese mono a solciitar"))
-cuotas = parseInt(prompt("ingrese cantidad de cuotas , no mayor a 20"))
+let cuotas = parseInt(prompt("ingrese cantidad de cuotas , no mayor a 20"))
 
 while(cuotas > 20 || cuotas < 0){
     cuotas = parseInt(prompt("ingrese en cuantas cuotas , no mayor a 20 ni menor a 0"))
 }
 if( cuotas <= 10){
-    monto = monto *1,1;
+    monto = monto *1.1;
+    console.log("cuotas" + monto)
 }else{
-    monto = monto * 1,2;
+    monto = monto * 1.2;
+    console.log("cuotas " + monto)
 }
 if (monto < 10000){
-    montototal = monto * 1,1;
+    montototal = monto * 1.1;
+    console.log("montototal" +montototal)
 }else if( monto > 10000 && monto < 20000){
     montototal = monto *1.15;
+    console.log("montototal" +montototal)
 }else{
-    montototal = monto * 1,25;
+    montototal = monto * 1.25;
+    console.log("monto" + montototal)
 }
-
-console.log(montototal);
