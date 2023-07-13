1e212fcc2680d7b8c60d6601302752e01164dc40
Mariano Lasarte <marianolasarte@hotmail.com.ar>
2023-07-13 13:10:46 -0300

    new commit

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ let montototal = 0
 let monto = parseFloat(prompt("ingrese mono a solciitar"))
 let cuotas = parseInt(prompt("ingrese cantidad de cuotas , no mayor a 20"))
 
+function pedircuotas(){
+    let cuotas = parseInt(prompt( " ingrese cantidad de cuotas , no mayor a 20"));
+
 while(cuotas > 20 || cuotas < 0){
     cuotas = parseInt(prompt("ingrese en cuantas cuotas , no mayor a 20 ni menor a 0"))
 }
+
+return cuotas; }
+
 if( cuotas <= 10){
     monto = monto *1.1;
     console.log("cuotas" + monto)
@@ -14,7 +20,7 @@ if( cuotas <= 10){
     monto = monto * 1.2;
     console.log("cuotas " + monto)
 }
-if (monto < 10000){
+if (monto <= 10000){
     montototal = monto * 1.1;
     console.log("montototal" +montototal)
 }else if( monto > 10000 && monto < 20000){
