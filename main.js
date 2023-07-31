c4af4ab0defd9b0e82da631c616e65d79a6058c8
Mariano Lasarte <marianolasarte@hotmail.com.ar>
2023-07-31 15:39:03 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -30,3 +30,23 @@ if (monto <= 10000){
     montototal = monto * 1.25;
     console.log("monto" + montototal)
 }
+
+const Servicio = function (tarjeta, mantenimiento, limite){
+   this.tarjeta = tarjeta
+   this.mantenimeinto = mantenimeinto
+   this.limite = limite 
+}
+
+let sercicio1 = new Servicio ("gold",7000, 500000)
+let servicio2 = new Servicio ("platino",11000, 1000000 )
+let servicio3 = new Servicio ("black", 19000, 2000000)
+
+let lista =[ servicio1,servicio2,servicio3 ]
+
+function flitrarServicio(){
+    let pedirtarjeta = prmopt( "Por cual tarjeta quiere consultar")
+    let resultado = lista.filter ((banco)=> banco.nombre.include(pedirtarjeta))
+}
+
+if(resultado = )
+
